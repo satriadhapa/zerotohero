7024d3e1639501a61fee0a3ad1ef68e5f3cf14fe
satriadhapa <satriadhapa10@gmail.com>
2025-09-09 09:23:52 +0700

    type data python

--- main.py
+++ main.py
@@ -1,3 +1,28 @@
 print("ini adalah tulisan pertama saya di python setelah sekian lama")
 # ini adalah komentar
 print(123)
+
+#type data string
+print("aku suka python")
+
+#type data integer dan string
+print("ini type data integer dan string : ", 12320)
+
+print(3.12) # type data float
+
+#type data hexadecimal
+print('9a')
+
+print(5j) #type data complex
+
+#type data list
+print([1, 2, 3, 4, 5])
+print(["satr", "dua"])
+
+#type data tuple
+print((1,2,65,3,4,3,42))
+
+print("=" *10)
+
+biodata = {"pekerjaan": "kepala", "umur" : 90}
+print(biodata["pekerjaan"])
